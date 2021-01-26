ae46c2161cd56624c26b6533a5400684256d6fa0
alo924 <alexandra.ochalek@kellogg.northwestern.edu>
2021-01-25 18:55:14 -0600

    update2

--- noober.js
+++ noober.js
@@ -16,7 +16,7 @@ function pageLoad() {
 
 console.log(levelOfService)
 
-if(ride.length>1){
+if (ride.length>1){
   passenger1Name = ride[0].passengerDetails.first + ' ' +ride[0].passengerDetails.last
   passenger1Phone = ride[0].passengerDetails.phoneNumber
   passenger1NumberofPassengers = ride[0].numberOfPassengers
@@ -33,7 +33,7 @@ if(ride.length>1){
   console.log(passenger1DropoffAddressLine1)
   console.log(passenger1DropoffAddressLine2)
 
-} else if (ride.length=2){
+} else if (ride.length=2) {
   
   passenger1Name = ride[0].passengerDetails.first + ' ' +ride[0].passengerDetails.last
   passenger1Phone = ride[0].passengerDetails.phoneNumber
